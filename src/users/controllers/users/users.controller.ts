24adc0d35dc8e5bbc006530723efe261b18bfca3
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-27 15:51:43 -0300

    primer commit

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from 'src/users/services/users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('say-hello')
+  getHello(): string {
+    return this.usersService.getHello();
+  }
+}
