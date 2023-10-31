e4c421b9811ffc6756580c45f4200e5c49372f7c
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-31 15:50:12 -0300

    Corrige el nombre de la clase

--- src/projects/entities/projects.entity.ts
+++ src/projects/entities/projects.entity.ts
@@ -3,7 +3,7 @@ import { IProject } from 'src/interfaces/project.interface';
 import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'projects' })
-export class UserEntity extends BaseEntity implements IProject {
+export class ProjectEntity extends BaseEntity implements IProject {
   @Column()
   name: string;
   @Column()
