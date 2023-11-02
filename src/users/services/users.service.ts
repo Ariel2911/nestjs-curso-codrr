3b338a286f21fcf6deafaf1cda1136d7c80bb6f7
Ariel Barrios <ariel29111978@hotmail.com>
2023-11-01 22:18:36 -0300

    Agrega createUser en users.service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/users.entity';
+import { Repository } from 'typeorm';
+import { UserDTO } from '../dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hola mundo';
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  public async createUser(body: UserDTO): Promise<UserEntity> {
+    try {
+      return await this.userRepository.save(body);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
